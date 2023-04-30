3bc3b692ee7763209122a9a4c311a233c3275d5d
preetsagar <ps792001@gmail.com>
2023-04-30 13:45:31 +0530

    initial commit

--- src/main/java/com/preet/task1/controller/controller.java
+++ src/main/java/com/preet/task1/controller/controller.java
@@ -2,20 +2,33 @@ package com.preet.task1.controller;
 
 import com.preet.task1.model.serverModel;
 import com.preet.task1.serverRepository;
+//import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import springfox.documentation.annotations.ApiIgnore;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
+@CrossOrigin(origins = "*")
 public class controller {
 
     @Autowired
     serverRepository repository;
+
+    @ApiIgnore
+    @RequestMapping(value="/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
 
     @GetMapping("/servers")
     public List<serverModel> getAllServer(){
@@ -39,12 +52,12 @@ public class controller {
         return repository.save(server);
     }
 
-    @DeleteMapping("servers/{id}")
+    @DeleteMapping("/servers/{id}")
     public void deleteServer(@PathVariable String id) {
         repository.deleteById(id);
     }
 
-    @GetMapping("servers/name/{name}")
+    @GetMapping("/servers/name/{name}")
     public List<serverModel> getServersByName(@PathVariable String name) {
         List<serverModel> servers = repository.findByName(name);
         if (servers.isEmpty()) {
